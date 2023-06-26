2b748f5578b0af4ace3945897f59d2e27e718c41
Jose Luis Garcia Aróstegui <jl.garcia@copermatica.com>
2023-06-26 16:01:17 +0200

    dia 26 mediodia

--- push_app/lib/main.dart
+++ push_app/lib/main.dart
@@ -4,11 +4,15 @@ import 'package:push_app/config/router/app_router.dart';
 import 'package:push_app/config/theme/app_theme.dart';
 import 'package:push_app/presentation/blocs/notifications/notifications_bloc.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized(); 
+  NotificationsBloc.initializeFCM;
+
   runApp(
     MultiBlocProvider(
         providers: [BlocProvider(create: (_) => NotificationsBloc())],
-        child: const MainApp()),
+        child: const MainApp(),),
   );
 }
 
--- push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
+++ push_app/lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:push_app/firebase_options.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 
 part 'notifications_event.dart';
 part 'notifications_state.dart';
@@ -9,6 +11,13 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
   NotificationsBloc() : super(const NotificationsState());
+
+
+  static Future<void> initializeFCM() async  {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
 
   void requestPermisions() async {
     NotificationSettings settings = await messaging.requestPermission(
