2fba371df5ae8d0180e2119de2bce35916cd1ace
Luis <Luis.garcia.arostegui@gmail.com>
2023-07-24 00:15:18 +0200

    fin seccion 27

--- teslo-app/lib/features/auth/presentation/providers/auth_provider.dart
+++ teslo-app/lib/features/auth/presentation/providers/auth_provider.dart
@@ -1,17 +1,28 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:teslo_shop/features/auth/domain/domain.dart';
 import 'package:teslo_shop/features/auth/infrastructure/infrastructure.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_impl.dart';
+import 'package:teslo_shop/features/shared/infrastructure/services/key_value_storage_service.dart';
 
 final authProvider = StateNotifierProvider<AuthNotifier, AuthState>((ref) {
   final authRepository = AuthRepositoryImpl();
+  final keyValueStorageService = KeyValueStorageServiceImpl();
 
-  return AuthNotifier(authRepository: authRepository);
+  return AuthNotifier(
+      authRepository: authRepository,
+      keyValueStorageService: keyValueStorageService);
 });
 
 class AuthNotifier extends StateNotifier<AuthState> {
   final AuthRepository authRepository;
+  final KeyValueStorageService keyValueStorageService;
 
-  AuthNotifier({required this.authRepository}) : super(AuthState());
+  AuthNotifier({
+    required this.authRepository,
+    required this.keyValueStorageService,
+  }) : super(AuthState()) {
+    checkAuthStatus();
+  }
 
   Future<void> loginUser(String email, String password) async {
     await Future.delayed(const Duration(milliseconds: 500));
@@ -31,10 +42,20 @@ class AuthNotifier extends StateNotifier<AuthState> {
 
   void registerUser(String email, String password) async {}
 
-  void checkAuthStatus() async {}
+  void checkAuthStatus() async {
+    final String? token =
+        await keyValueStorageService.getValue<String>('token');
+    if (token == null) return logout();
+    try {
+      final user = await authRepository.checkAuthStatus(token);
+      _setLoggedUser(user);
+    } catch (e) {
+      logout();
+    }
+  }
 
-  void _setLoggedUser(User user) {
-    // TODO: necesito guardar el token físicamente
+  void _setLoggedUser(User user) async {
+    await keyValueStorageService.setKeyValue('token', user.token);
     state = state.copyWith(
       user: user,
       authStatus: AuthStatus.authenticated,
@@ -43,7 +64,7 @@ class AuthNotifier extends StateNotifier<AuthState> {
   }
 
   Future<void> logout([String? errorMessage]) async {
-    // TODO: limpiar token
+    await keyValueStorageService.removeKey('token');
     state = state.copyWith(
         authStatus: AuthStatus.notAuthenticated,
         user: null,
--- teslo-app/lib/features/shared/infrastructure/services/key_value_storage_impl.dart
+++ teslo-app/lib/features/shared/infrastructure/services/key_value_storage_impl.dart
@@ -1,27 +1,46 @@
 import 'package:shared_preferences/shared_preferences.dart';
-
 import 'key_value_storage_service.dart';
 
 class KeyValueStorageServiceImpl extends KeyValueStorageService {
-  Future getSharedPrefs() async {
+  Future<SharedPreferences> getSharedPrefs() async {
     return SharedPreferences.getInstance();
   }
 
   @override
-  Future<T?> getValue<T>(String key) {
-    // TODO: implement getValue
-    throw UnimplementedError();
+  Future<T?> getValue<T>(String key) async {
+    final prefs = await getSharedPrefs();
+    switch (T) {
+      case int:
+        return prefs.getInt(key) as T?;
+      case String:
+        return prefs.getString(key) as T?;
+      default:
+        throw UnimplementedError(
+            'Setter no implementado para el tipo${T.runtimeType}');
+    }
   }
 
   @override
-  Future<bool> removeKey(String key) {
-    // TODO: implement removeKey
-    throw UnimplementedError();
+  Future<bool> removeKey(String key) async {
+    final prefs = await getSharedPrefs();
+    return await prefs.remove(key);
   }
 
   @override
-  Future<void> setKeyValue<T>(String key, T value) {
-    // TODO: implement setKeyValue
-    throw UnimplementedError();
+  Future<void> setKeyValue<T>(String key, T value) async {
+    final prefs = await getSharedPrefs();
+    switch (T) {
+      case int:
+        prefs.setInt(key, value as int);
+        break;
+
+      case String:
+        prefs.setString(key, value as String);
+        break;
+
+      default:
+        throw UnimplementedError(
+            'Setter no implementado para el tipo${T.runtimeType}');
+    }
   }
 }
