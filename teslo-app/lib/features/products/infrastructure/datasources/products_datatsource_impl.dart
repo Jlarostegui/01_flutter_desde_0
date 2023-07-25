1d6904146b6fa44d6d84ff1dce9ce0a28be78515
Luis <Luis.garcia.arostegui@gmail.com>
2023-07-26 00:19:31 +0200

    añadidas impl

---
+++ teslo-app/lib/features/products/infrastructure/datasources/products_datatsource_impl.dart
@@ -0,0 +1,50 @@
+import 'package:dio/dio.dart';
+import 'package:teslo_shop/config/config.dart';
+import 'package:teslo_shop/features/products/domain/domain.dart';
+
+class ProductsDataSourceImpl extends ProductsDataSource {
+  late final Dio dio;
+  final String accessToken;
+
+  ProductsDataSourceImpl({
+    required this.accessToken,
+  }) : dio = Dio(
+          BaseOptions(
+            baseUrl: Environment.apiUrl,
+            headers: {
+              'Content-Type': 'text/plain',
+              'Authorization': 'Bearer $accessToken'
+            },
+          ),
+        );
+
+  @override
+  Future<Product> createUpdateProduct(Map<String, dynamic> productLike) {
+    // TODO: implement createUpdateProduct
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Product> getProductById(String id) {
+    // TODO: implement getProductById
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Product>> getProductByPage(
+      {int limit = 10, int offset = 0}) async {
+    final response =
+        await dio.get<List>('api/products?limit=$limit&offset=$offset');
+    final List<Product> products = [];
+    for (final product in response.data ?? []) {
+      products.add(product);
+    }
+    return products;
+  }
+
+  @override
+  Future<List<Product>> searchProductByTerm(String term) {
+    // TODO: implement searchProductByTerm
+    throw UnimplementedError();
+  }
+}
---
+++ teslo-app/lib/features/products/infrastructure/repositories/products_repository_impl.dart
@@ -0,0 +1,26 @@
+import 'package:teslo_shop/features/products/domain/domain.dart';
+
+class ProductsRepositoryImpl extends ProductsRepository {
+  final ProductsDataSource dataSource;
+  ProductsRepositoryImpl(this.dataSource);
+
+  @override
+  Future<Product> createUpdateProduct(Map<String, dynamic> productLike) {
+    return dataSource.createUpdateProduct(productLike);
+  }
+
+  @override
+  Future<Product> getProductById(String id) {
+    return dataSource.getProductById(id);
+  }
+
+  @override
+  Future<List<Product>> getProductByPage({int limit = 10, int offset = 0}) {
+    return dataSource.getProductByPage(limit: limit, offset: offset);
+  }
+
+  @override
+  Future<List<Product>> searchProductByTerm(String term) {
+    return dataSource.searchProductByTerm(term);
+  }
+}
