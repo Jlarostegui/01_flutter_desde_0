9d1c9607ebb6202fdd8238efc6db9f3f709c9998
Luis <Luis.garcia.arostegui@gmail.com>
2023-07-29 19:37:47 +0200

    fin seccion 28

--- teslo-app/lib/features/products/infrastructure/datasources/products_datatsource_impl.dart
+++ teslo-app/lib/features/products/infrastructure/datasources/products_datatsource_impl.dart
@@ -35,7 +35,7 @@ class ProductsDataSourceImpl extends ProductsDataSource {
   Future<List<Product>> getProductByPage(
       {int limit = 10, int offset = 0}) async {
     final response =
-        await dio.get<List>('api/products?limit=$limit&offset=$offset');
+        await dio.get<List>('/products?limit=$limit&offset=$offset');
     final List<Product> products = [];
     for (final product in response.data ?? []) {
       products.add(
---
+++ teslo-app/lib/features/products/presentation/widgets/product_card.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:teslo_shop/features/products/domain/domain.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({super.key, required this.product});
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _ImageViewer(images: product.images),
+        Text(product.title, textAlign: TextAlign.center),
+        const SizedBox(
+          height: 20,
+        ),
+      ],
+    );
+  }
+}
+
+class _ImageViewer extends StatelessWidget {
+  const _ImageViewer({required this.images});
+
+  final List<String> images;
+
+  @override
+  Widget build(BuildContext context) {
+    if (images.isEmpty) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.asset('assets/images/no-image.jpg', fit: BoxFit.cover),
+      );
+    }
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: FadeInImage(
+        fit: BoxFit.cover,
+        height: 250,
+        fadeOutDuration: const Duration(milliseconds: 500),
+        image: NetworkImage(images.first),
+        placeholder: const AssetImage('assets/loaders/bottle-loader.gif'),
+      ),
+    );
+  }
+}
